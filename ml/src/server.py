788ab6b4a806a2294059975bec2b85ef666cd48b
Jakob Ström <jakob.strom1@icloud.com>
2025-11-06 22:33:07 +0100

    update of port specification for deployment

--- ml/src/server.py
+++ ml/src/server.py
@@ -8,6 +8,7 @@ import utils as u
 from typing import Literal, Any
 from search_OA_api import movieSuggestion
 import omdb_api as o
+import os
 
 app = Flask(__name__)
 CORS(app)  # Allow requests from React
@@ -84,4 +85,5 @@ def interpret_extra_user_input(user_input: dict[str, Any]) -> dict[str, Any]:
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=False)
